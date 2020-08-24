44c12d9bee9a9a35df381f334877e61ad6177368
yuyinghan <johnyucn@163.com>
2020-08-24 16:42:41 +0800

    first

---
+++ src/main/java/cn/johnyu/cloudmain/CloudMainApplication.java
@@ -0,0 +1,13 @@
+package cn.johnyu.cloudmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMainApplication.class, args);
+    }
+
+}
